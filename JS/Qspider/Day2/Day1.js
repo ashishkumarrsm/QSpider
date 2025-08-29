786b86a1a7fb2aecb2a93555281f482f43c5bd07
Ashish Kumar <ashishkumar141199@gmail.com>
2025-08-29 12:38:48 +0530

    js token and html hestory

---
+++ JS/Qspider/Day2/Day1.js
@@ -0,0 +1,152 @@
+// console.log("Hello without setTimeOut");
+
+// //  in node js is a run time envarnment to run the js program its devloped on v8 + c++ language 
+
+// // how to conact over js to html
+// //  we have two type of that 
+// // 1. internal js
+// // 2.external js 
+
+// // 1. internal js
+
+// // for that we can use the script tag 
+
+// let TimeOut= setTimeout(() => {
+//     console.log("hello with setTimeout")
+// }, 2000);
+
+
+// document.write("this is data <br/>")
+// document.writeln("this is data<br/>")
+
+
+// // ln are give the spage at the last but wright are not give any space 
+
+
+
+
+
+
+
+
+// document.writeln("this is datapppppppppppppppppp")
+
+// // let age= parseInt(prompt("enter The name age then i will tell you you can vote or not"))
+
+
+// // if(age>=18)
+// //     console.log("you can vot")
+// // else console.log("you can't vote")
+
+
+
+
+// // console is the object in js 
+
+// console.log(console);
+// // thay have loot of function 
+// // assert
+// // : 
+// // ƒ assert()
+// // clear
+// // : 
+// // ƒ clear()
+// // context
+// // : 
+// // ƒ context()
+// // count
+// // : 
+// // ƒ count()
+// // countReset
+// // : 
+// // ƒ countReset()
+// // createTask
+// // : 
+// // ƒ createTask()
+// // debug
+// // : 
+// // ƒ debug()
+// // dir
+// // : 
+// // ƒ dir()
+// // dirxml
+// // : 
+// // ƒ dirxml()
+// // error
+// // : 
+// // ƒ error()
+// // group
+// // : 
+// // ƒ group()
+// // groupCollapsed
+// // : 
+// // ƒ groupCollapsed()
+// // groupEnd
+// // : 
+// // ƒ groupEnd()
+// // info
+// // : 
+// // ƒ info()
+// // log
+// // : 
+// // ƒ log()
+// // memory
+// // : 
+// // MemoryInfo {totalJSHeapSize: 239000000, usedJSHeapSize: 188000000, jsHeapSizeLimit: 3760000000}
+// // profile
+// // : 
+// // ƒ profile()
+// // profileEnd
+// // : 
+// // ƒ profileEnd()
+// // table
+// // : 
+// // ƒ table()
+// // time
+// // : 
+// // ƒ time()
+// // timeEnd
+// // : 
+// // ƒ timeEnd()
+// // timeLog
+// // : 
+// // ƒ timeLog()
+// // timeStamp
+// // : 
+// // ƒ timeStamp()
+// // trace
+// // : 
+// // ƒ trace()
+// // warn
+// // : 
+// // ƒ warn()
+// // Symbol(Symbol.toStringTag)
+// // : 
+// // "console"
+// console.error("error");
+
+
+
+// //  tokens in js 
+
+// //      In JavaScript, a token is the smallest unit of the language that the JavaScript engine can recognize, such as keywords, operators, identifiers, literals, or symbols
+// // What is a Token in JavaScript
+// Tokens include words, numbers, symbols (like +, -, =), and reserved keywords (like var, if, else).
+
+// When JavaScript parses code, it breaks the source text into these tokens to understand and execute logic.
+
+// Types of Tokens
+// Identifiers: Names for variables and functions (e.g., myVar)
+
+// Keywords: Reserved words for programming constructs (e.g., if, else, var)
+
+// Literals: Constant values (e.g., numbers 5, strings 'Hello')
+
+// Operators: Symbols for operations (e.g., +, =, &&)
+
+// Separators: Symbols that separate statements or blocks (e.g., ;, ,, ())
+
+// Use Cases
+// Tokens are fundamental for JavaScript parsing and error detection, such as “unexpected token” syntax errors when the parser finds an out-of-place token.
+
+// In authentication, “token” also means authentication credentials like JWTs (JSON Web Tokens), which are used to securely transmit information between parties and maintain user sessions.
