23e0917e41c51cf31d2f8f33f583ed418c24bba7
Ashish Kumar <ashishkumar141199@gmail.com>
2025-08-30 13:02:55 +0530

    html tages and data type in js

---
+++ JS/Qspider/Day3/script.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+
+// primitive data type means thay store the single value data 
+// or primitive data types are immutable in nature 
+// that means we can't change or modify tare values 
+// types of primtive data type 
+// number
+// booliun
+// string
+// null
+// undifind
+// bigint 
+// symbol
