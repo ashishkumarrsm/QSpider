053ed212dd9f78d10456c9edc0c8473b719563f2
Ashish Kumar <ashishkumar141199@gmail.com>
2025-09-03 10:55:24 +0530

    type casting in js and in html we see the formatting tag

---
+++ JS/Qspider/day5/index.js
@@ -0,0 +1,15 @@
+console.log(10+"10");
+console.log(10-"10");
+console.log("10"+10-20);
+
+console.log(typeof(10+"10"))
+console.log(typeof(10-"10"))
+console.log(typeof("10"+10-20))
+console.log(10-'100a')
+console.log(10-Number('100RS'))
+console.log(10-parseInt('100RS'))
+console.log(10-'100')
+console.log(100+Number('100'))
+console.log(Boolean(100)+false)
+console.log(Boolean("hello")+false)
+console.log(Boolean("")+false)
