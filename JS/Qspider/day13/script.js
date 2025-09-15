e8ea3bfdfeed94f1548caaf23673b9c6a51e49fd
Ashish Kumar <ashishkumar141199@gmail.com>
2025-09-15 12:30:11 +0530

    array and array methode and in html we will learn table and iframe etc

---
+++ JS/Qspider/day13/script.js
@@ -0,0 +1,67 @@
+// // array leterl
+
+// let arr = [
+//   10,
+//   20,
+//   30.4,
+//   "hello",
+//   undefined,
+//   () => {
+//     // console.log("hllo inside the function ");
+//   },
+//   { ename: "Ashish Kumar" },
+//   [10, 20, 30, 40, 50],
+// ];
+
+// // console.log(arr[4])
+// // console.log(arr[6])
+// console.log(arr)
+// console.log(arr[5])
+
+// array constructure
+
+// when we pass 1 parameter tay consider as a lengthe of array but when we pass multiple value as paramer then tay treted as array values
+
+// let arr1=Array(10)
+
+// let arr1=Array(10,23,23,()=>{})
+
+// console.log(arr1)
+
+// array methode
+
+let arr = [20, 30, 49];
+
+// push methode use too inser the elemet at the end of array
+// like
+
+// arr.push(100,33,45)
+// console.log(arr.push(100, 33, 45));
+// push methode retun the lengthe of an array
+// console.log(arr);
+
+// arr.unshift(20, 30, 3393);
+// console.log(arr.unshift(20, 30, 3393));
+
+// console.log(arr)
+
+// unsfit methode are use to inser the value inside the array butr at the starting of array 
+
+
+// arr.pop()
+// console.log(arr.pop())
+// console.log(arr)
+// arr.shift()
+// console.log(arr.shift())
+// console.log(arr)
+
+
+
+console.log(arr.indexOf(20))
+
+console.log(arr.includes(20))
+console.log(arr.at(2))
+console.log(arr.lastIndexOf(20))
+console.log(arr.slice(2,3))
+console.log(arr)
+console.log(arr.splice(1,3))
